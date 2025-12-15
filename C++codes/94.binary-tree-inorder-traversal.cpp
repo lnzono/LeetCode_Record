17cb084638afe5408e1b3643542abdb4bf8664e9
zhangxule <zhangxule20020105@outlook.com>
2025-12-15 22:50:00 +0800

    94. 二叉树的中序遍历
    Signed-off-by: zhangxule <zhangxule20020105@outlook.com>

---
+++ C++codes/94.binary-tree-inorder-traversal.cpp
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=cpp
+ * @lcpr version=30204
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    vector<int> inorderTraversal(TreeNode *root)
+    {
+        vector<int> vet;
+        stack<TreeNode *> st;
+        while (root || !st.empty())
+        {
+            if (root)
+            {
+                st.push(root);
+                root = root->left;
+            }
+            else
+            {
+                root = st.top();
+                vet.push_back(root->val);
+                st.pop();
+                root = root->right;
+            }
+        }
+        return vet;
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [1,null,2,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+ */
