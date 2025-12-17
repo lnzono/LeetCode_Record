4d54b0cfb7b5da4f3019c836166a2a845b0fddc3
zhangxule <zhangxule20020105@outlook.com>
2025-12-17 21:52:07 +0800

    [102] 二叉树的层序遍历
    Signed-off-by: zhangxule <zhangxule20020105@outlook.com>

---
+++ C++codes/102.binary-tree-level-order-traversal.cpp
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=cpp
+ * @lcpr version=30204
+ *
+ * [102] 二叉树的层序遍历
+ */
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    vector<vector<int>> levelOrder(TreeNode *root)
+    {
+        vector<vector<int>> arr;
+
+        if (!root)
+            return arr;
+        queue<TreeNode *> qu;
+        qu.push(root);
+        while (!qu.empty())
+        {
+            vector<int> vet;
+            // 后面的size ,会动态变化，所以需要保存
+            int queue_size = qu.size();
+            for (int i = 0; i < queue_size; i++)
+            {
+                root = qu.front();
+                qu.pop();
+                vet.push_back(root->val);
+                if (root->left)
+                    qu.push(root->left);
+                if (root->right)
+                    qu.push(root->right);
+            }
+            arr.push_back(vet);
+        }
+        return arr;
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [3,9,20,null,null,15,7]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+ */
