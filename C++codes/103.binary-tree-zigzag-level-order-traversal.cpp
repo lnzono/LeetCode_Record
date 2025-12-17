583248f457cbc488f781fb3dffadec9520bdb954
zhangxule <zhangxule20020105@outlook.com>
2025-12-17 22:13:19 +0800

    [103] 二叉树的锯齿形层序遍历
    Signed-off-by: zhangxule <zhangxule20020105@outlook.com>

---
+++ C++codes/103.binary-tree-zigzag-level-order-traversal.cpp
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode.cn id=103 lang=cpp
+ * @lcpr version=30204
+ *
+ * [103] 二叉树的锯齿形层序遍历
+ */
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode *root)
+    {
+        vector<vector<int>> arr;
+        if (!root)
+            return arr;
+        queue<TreeNode *> qu;
+        qu.push(root);
+        bool leftToRight = true;
+        while (!qu.empty())
+        {
+            // 后面的size ,会动态变化，所以需要保存
+            int queue_size = qu.size();
+            vector<int> vet(queue_size);
+            for (int i = 0; i < queue_size; i++)
+            {
+                root = qu.front();
+                qu.pop();
+                int index = leftToRight ? i : queue_size - 1 - i;
+                vet[index] = root->val;
+
+                if (root->left)
+                    qu.push(root->left);
+                if (root->right)
+                    qu.push(root->right);
+            }
+            arr.push_back(vet);
+            leftToRight = !leftToRight;
+        }
+        return arr;
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [3,9,20,null,null,15,7]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+ */
